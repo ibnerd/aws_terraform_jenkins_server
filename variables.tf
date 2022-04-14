40582a490d32a8d3b3f81199fbf9db2ff81c8b9a
ibnerd <faulkner.j414@gmail.com>
2022-04-14 10:45:27 -0500

    currently working need to add a way to bind ssl/tls certs to jenkins server.

--- variables.tf
+++ variables.tf
@@ -2,3 +2,17 @@ variable "aws_region" {
   description = "AWS region to create resources"
   default     = "us-east-1"
 }
+
+variable "vpc_id" {
+  description = "AWS VPC ID where ec2 will be launched"
+  
+}
+
+variable "cidr_block" {
+  description = "my personal IP"
+  
+}
+
+variable "key_name" {
+  description = "value"
+}
